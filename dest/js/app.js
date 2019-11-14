eef3885a4d6120534f74f8132308631b5fe1f55d
MrHidalgo <Luckyhidalgo@gmail.com>
2019-11-14 09:11:14 +0200

    Homepage - added main animation.

--- dest/js/app.js
+++ dest/js/app.js
@@ -157,6 +157,62 @@ $(document).ready(function (ev) {
       });
     }
   };
+
+  var initMainAnimation = function initMainAnimation() {
+    var tl = new TimelineMax();
+
+    var _winW = $(window).width(),
+        _winH = $(window).height();
+
+    var _header = $('header'),
+        _mainOverlay = $('[main-overlay-js]'),
+        _mainBoxWrapper = $('[main-boxWrapper-js]'),
+        _mainTitle = $('[main-title-js]'),
+        _mainTitleLine = $('[main-title-line-js]'),
+        _mainTitleText = $('[main-title-text-js] span');
+
+    if (_winW > 767) {
+      tl.set(_header, {
+        transformOrigin: 'center',
+        top: 0,
+        left: -100
+      });
+    } else {
+      tl.set(_header, {
+        transformOrigin: 'center',
+        left: 0,
+        top: -100
+      });
+    }
+
+    tl.set(_mainBoxWrapper, {
+      transformOrigin: 'center',
+      bottom: -100
+    });
+
+    var _titleLineWidth = 0,
+        _titleOffsetY = 0;
+
+    if (_winW > 1023) {
+      _titleLineWidth = 80;
+    } else {
+      _titleLineWidth = 50;
+    }
+
+    if (_winW > 1279 && _winH > 767) {
+      _titleOffsetY = '110%';
+    } else if (_winW > 1023 && _winH > 767) {
+      _titleOffsetY = '100%';
+    } else if (_winW < 375 && _winH < 569) {
+      _titleOffsetY = '42%';
+    } else if (_winW < 424 && _winH < 668) {
+      _titleOffsetY = '60%';
+    } else {
+      _titleOffsetY = '75%';
+    }
+
+    tl.to(_header, 0.5, { opacity: 1, left: 0, top: 0, ease: Power1.easeInOut }).to(_mainBoxWrapper, 0.5, { opacity: 1, bottom: 0, ease: Power1.easeInOut }, '-=0.2').to(_mainTitleLine, 0.35, { opacity: 1, width: _titleLineWidth, ease: Power1.easeInOut }).staggerTo(_mainTitleText, 0.75, { opacity: 1, ease: Power1.easeInOut }, 0.035).to(_mainTitle, 0.5, { scale: 0.55, y: _titleOffsetY, transformOrigin: 'left', ease: Power1.easeInOut }).to(_mainOverlay, 0.5, { opacity: 0, ease: Power1.easeInOut }, '-=0.5');
+  };
   /*
   * CALLBACK :: end
   * ============================================= */
@@ -176,6 +232,7 @@ $(document).ready(function (ev) {
 
     // callback
     initVideo();
+    initMainAnimation();
     // ==========================================
   };
   initJquery();
--- src/js/_document/indexJquery.js
+++ src/js/_document/indexJquery.js
@@ -41,6 +41,75 @@ $(document).ready((ev) => {
       });
     }
   };
+
+  const initMainAnimation = () => {
+    const tl = new TimelineMax();
+
+    const _winW = $(window).width(),
+      _winH = $(window).height();
+
+    const _header = $('header'),
+      _mainOverlay = $('[main-overlay-js]'),
+      _mainBoxWrapper = $('[main-boxWrapper-js]'),
+      _mainTitle = $('[main-title-js]'),
+      _mainTitleLine = $('[main-title-line-js]'),
+      _mainTitleText = $('[main-title-text-js] span');
+
+    if(_winW > 767) {
+      tl.set(
+        _header, {
+          transformOrigin: 'center',
+          top: 0,
+          left: -100
+        }
+      );
+    } else {
+      tl.set(
+        _header, {
+          transformOrigin: 'center',
+          left: 0,
+          top: -100
+        }
+      );
+    }
+
+    tl.set(
+      _mainBoxWrapper, {
+        transformOrigin: 'center',
+        bottom: -100
+      }
+    );
+
+
+    let _titleLineWidth = 0,
+      _titleOffsetY = 0;
+
+    if(_winW > 1023) {
+      _titleLineWidth = 80;
+    } else {
+      _titleLineWidth = 50
+    }
+
+    if(_winW > 1279 && _winH > 767) {
+      _titleOffsetY = '110%';
+    } else if(_winW > 1023 && _winH > 767) {
+      _titleOffsetY = '100%';
+    } else if(_winW < 375 && _winH < 569) {
+      _titleOffsetY = '42%';
+    } else if(_winW < 424 && _winH < 668) {
+      _titleOffsetY = '60%';
+    } else {
+      _titleOffsetY = '75%';
+    }
+
+    tl
+      .to(_header, 0.5, {opacity:1, left: 0, top: 0, ease: Power1.easeInOut})
+      .to(_mainBoxWrapper, 0.5, {opacity:1, bottom: 0, ease: Power1.easeInOut}, '-=0.2')
+      .to(_mainTitleLine, 0.35, {opacity: 1, width: _titleLineWidth, ease: Power1.easeInOut})
+      .staggerTo(_mainTitleText, 0.75, {opacity: 1, ease: Power1.easeInOut}, 0.035)
+      .to(_mainTitle, 0.5, {scale: 0.55, y:_titleOffsetY, transformOrigin: 'left', ease: Power1.easeInOut})
+      .to(_mainOverlay, 0.5, {opacity: 0, ease: Power1.easeInOut}, '-=0.5');
+  };
 	/*
 	* CALLBACK :: end
 	* ============================================= */
@@ -62,6 +131,7 @@ $(document).ready((ev) => {
 
     // callback
     initVideo();
+    initMainAnimation();
 		// ==========================================
   };
   initJquery();
